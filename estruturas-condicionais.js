efc7a1329b158d2c765890d96e8a63d832fa2130
MATHEUS PEREIRA DE SOUZA <matheussouza2393@digitalcollege.internal>
2026-09-25 11:52:54 -0300

    exercicio

---
+++ estruturas-condicionais.js
@@ -0,0 +1,66 @@
+/* function calcularIdade(){
+    let idade = document.querySelector("#idade").value;
+    if(idade.length > 0){
+        if(idade >= 18){
+            alert("Maior de idade");
+    } else {
+        alert("Menor de idade");
+    }
+} else {
+    alert("Digite um numero valido")
+}
+}
+
+let numero = 10;
+
+if(numero > 0){
+    console.log("É Positivo");
+} else if(numero < 0){
+    console.log("É Negativo");
+} else{
+    console.log("É Zero");
+} */
+/*  function parImpar(){
+    let number = document.querySelector("#number").value;
+    if(number % 2 == 0);
+        console.log("É par");
+    
+} */
+/* 
+let numero = 5;
+
+if(number % 2 == 0){
+    console.log(`o numero ${numero} é par`);
+    
+} else{
+    console.log(`o numero ${numero} é impar`);
+}
+     */
+/*  let notaA = 10;
+ let notaB = 8;
+ let notaC = 7;
+ let media = (notaA + notaB + notaC) / 3;
+ console.log('sua media é: ${media.tofixed(1)}');
+ if(media >= 7){
+    console.log("Aprovado");
+ } else if (media < && media >= 5){
+    console.log("Recuperação");
+ } else{
+    console.log("Esta de recuperação");
+ }
+ */
+ 
+ let valor = 100;
+ let porcentagem = 10;
+ let desconto = (valor * porcentagem) / 100;
+
+ if (valor >= 100){
+    console.log{`Sua compra de R$ ${valor} recebeu um desconto de ${porcentagem}, e ficou ${valor - desconto}`}
+ }
+
+ let numero = 67;
+ if(numero >= 10 && numero <=50 ){
+    log("esta no intervalo")
+ } else{
+    log("nao esta no intervalo")
+ }
